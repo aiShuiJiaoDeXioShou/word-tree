a69f043735ef58180abcdbf72a416a8ef5571e23
aiShuiJiaoDeXioShou <2832294398@qq.com>
2022-03-31 09:16:26 +0800

    变更

--- src/main/java/com/wordtree/Main.java
+++ src/main/java/com/wordtree/Main.java
@@ -1,12 +1,7 @@
 package com.wordtree;
 
-import com.google.gson.Gson;
 import com.wordtree.wt_kt_module.Coder;
-import com.wordtree.wt_kt_module.Login;
 import javafx.application.Application;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
 
 public class Main {
 
