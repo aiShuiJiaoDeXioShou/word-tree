b8e50b62e76ac1a03174acc66209ca2a71d9ed08
aiShuiJiaoDeXioShou <2832294398@qq.com>
2022-04-06 03:08:27 +0800

    3.7版本更新

--- src/main/java/com/wordtree/wt_physical/writerPj.java
+++
@@ -1,9 +0,0 @@
-package com.wordtree.wt_physical;
-
-public class writerPj {
-    //    用户方面
-    String name;
-    String mima;
-    String sex;
-    String denji;
-}
--- src/main/java/com/wordtree/wt_test/test.kt
+++
@@ -1,9 +0,0 @@
-package com.wordtree.wt_test
-
-class test {
-
-}
-
-fun main() {
-    println(60/60*60)
-}
